8deee9f08da5e9625fa1bf6b4ea285f7431050c3
janu-2002-ts <147641506+janu-2002-ts@users.noreply.github.com>
2026-03-24 13:22:09 +0530

    Add DBConnection class for MySQL database connection

---
+++ DBConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection 
+{
+    public static Connection getConnection() 
+    {
+        Connection con = null;
+
+        try 
+        {
+            con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/studentdb",
+                "root",
+                "password"
+            );
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e);
+        }
+
+        return con;
+    }
+}
